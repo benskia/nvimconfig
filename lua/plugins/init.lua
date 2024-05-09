18c91fcb9c0bb38b12883fa7ec956ab768b9abde
benskia <benski.abraham@gmail.com>
2024-05-09 18:41:12 -0500

    add git-signs plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-lua/plenary.nvim",
 
+  { "lewis6991/gitsigns.nvim",             opts = {} },
+
   {
     "folke/todo-comments.nvim",
     event = "VimEnter",
@@ -8,18 +10,18 @@ return {
     opts = { signs = false },
   },
 
-  { "folke/zen-mode.nvim", opts = {} },
-  { "folke/twilight.nvim", opts = {} },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  { "folke/zen-mode.nvim",                 opts = {} },
+  { "folke/twilight.nvim",                 opts = {} },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {} },
 
-  { "echasnovski/mini.align", version = "*", opts = {} },
-  { "echasnovski/mini.comment", version = "*", opts = {} },
-  { "echasnovski/mini.indentscope", version = "*", opts = {} },
-  { "echasnovski/mini.move", version = "*", opts = {} },
-  { "echasnovski/mini.pairs", version = "*", opts = {} },
-  { "echasnovski/mini.splitjoin", version = "*", opts = {} },
-  { "echasnovski/mini.statusline", version = "*", opts = {} },
-  { "echasnovski/mini.surround", version = "*", opts = {} },
+  { "echasnovski/mini.align",              version = "*", opts = {} },
+  { "echasnovski/mini.comment",            version = "*", opts = {} },
+  { "echasnovski/mini.indentscope",        version = "*", opts = {} },
+  { "echasnovski/mini.move",               version = "*", opts = {} },
+  { "echasnovski/mini.pairs",              version = "*", opts = {} },
+  { "echasnovski/mini.splitjoin",          version = "*", opts = {} },
+  { "echasnovski/mini.statusline",         version = "*", opts = {} },
+  { "echasnovski/mini.surround",           version = "*", opts = {} },
 
   "tpope/vim-fugitive",
   "tpope/vim-repeat",
